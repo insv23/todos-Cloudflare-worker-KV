84b73b9eb37086ebaf6699bf4cfa9f29e5b06f74
insv <insv23@outlook.com>
2024-05-29 18:12:14 +0800

    Add style

--- src/index.js
+++ src/index.js
@@ -15,14 +15,19 @@ const html = todos => `
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Todos</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
   </head>
-  <body>
-    <h1>Todos</h1>
-    <div>
-    	<input type="text" name="name" placeholder="A new todo">
-    	<button id="create">Create</button>
-    </div>
-    <div id="todos"></div>
+  <body class="bg-blue-100">
+  	<div class="w-full h-full flex content-center justify-center mt-8">
+  		<div class="bg-white shadow-md rounded px-8 pt-6 py-8 mb-4">
+				<h1 class="block text-grey-800 text-md font-bold mb-2">Todos</h1>
+				<div class="flex">
+					<input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-800 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" placeholder="A new todo">
+					<button class="bg-blue-500 hover:bg-blue-800 text-white font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" id="create">Create</button>
+				</div>
+				 <div class="mt-4" id="todos"></div>
+  		</div>
+  	</div>
   </body>
   <script>
 		window.todos = ${todos};
@@ -53,12 +58,15 @@ const html = todos => `
 			todoContainer.innerHTML = '';
     	window.todos.forEach(todo => {
         const el = document.createElement('div');
+        el.className = "border-t py-4"
         el.dataset.todo = todo.id;
 
 				const name = document.createElement('span');
+				name.className = todo.completed ? "line-through" : ""
 				name.textContent = todo.name;
 
 				const checkbox = document.createElement('input');
+				checkbox.className = "mx-4"
 				checkbox.type = 'checkbox';
 				checkbox.checked = !!todo.completed;
 				checkbox.addEventListener('click', toggleCheckbox);
